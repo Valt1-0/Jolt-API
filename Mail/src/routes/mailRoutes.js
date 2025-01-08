55306ffca8c6c19f4e6fc1035a4f30341d172166
Valt1-0 <lai2val1@gmail.com>
2025-01-08 14:37:33 +0100

    Ajout de la fonction d'envoi d'email de vérification et mise à jour des routes d'authentification. Commentaires et ports de MongoDB désactivés dans docker-compose.

--- Mail/src/routes/mailRoutes.js
+++ Mail/src/routes/mailRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { sendMail, sendRegisterConfirmationMail } = require("../controller/mailController");
 
-router.post("/send", sendRegisterConfirmationMail);
+router.post("/registerConfirmation", sendRegisterConfirmationMail);
 
 
 module.exports = router;
