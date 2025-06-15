5768951cd4d8053d977ab4530dca3df218b9b1c5
MitryDim <dimcorb@gmail.com>
2025-06-15 23:56:43 +0200

    fix: Correction de l'envoi d'e-mails pour gérer les erreurs de manière appropriée

--- Mail/src/controller/mailController.js
+++ Mail/src/controller/mailController.js
@@ -23,7 +23,7 @@ exports.sendRegisterConfirmationMail = async (data) => {
       .replace("{{username}}", username)
       .replace(
         "{{confirmationLink}}",
-        `http://localhost:5000/users/verifyEmail?token=${verificationToken}`
+        `http://localhost:8000/verifyEmail?token=${verificationToken}`
       );
 
     const mailOptions = {
