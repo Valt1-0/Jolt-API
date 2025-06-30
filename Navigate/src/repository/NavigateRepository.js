9ea7a750d3e48417ba3131b4071a202d2781fb36
MitryDim <dimcorb@gmail.com>
2025-06-30 16:55:56 +0200

    Update

--- Navigate/src/repository/NavigateRepository.js
+++ Navigate/src/repository/NavigateRepository.js
@@ -6,6 +6,7 @@ exports.create = (data) => Navigation.create(data);
 exports.findById = (id) => Navigation.findById(id);
 
 exports.findAll = async (filter = {}, page = 1, limit = 10) => {
+  console.log("Filtre géographique", filter);
   const pipeline = [];
 
   if (filter.lat && filter.lon && filter.radius) {
@@ -39,7 +40,7 @@ exports.findAll = async (filter = {}, page = 1, limit = 10) => {
   if (typeof filter.isGroup === "string") {
     filter.isGroup = filter.isGroup === "true";
   }
-  console.log("Filtre géographique", filter);
+
   if (Object.keys(filter).length > 0) {
     pipeline.push({ $match: filter });
   }
@@ -98,7 +99,7 @@ exports.findAll = async (filter = {}, page = 1, limit = 10) => {
   ]);
 
   const total = totalResult[0] ? totalResult[0].total : 0;
-
+console.log("Total navigations:", total,results);
   return { total, navigations: results };
 };
 
